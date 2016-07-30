3e34bd642f68edb65cefa030fc8de645972854eb
codenewbie <18267853511@163.com>
2016-07-30 22:50:44 +0800

    Long完成

---
+++ src/main/java/com/sedion/mynawang/java8/lang/TLong.java
@@ -0,0 +1,12 @@
+package com.sedion.mynawang.java8.lang;
+
+/**
+ * Long源码阅读和使用（JDK1.8）
+ * @auther mynawang
+ * @create 2016-07-30 16:16
+ */
+public class TLong {
+    // 与Integer类似，此处不列举
+
+    // LongCache方法high是定值，不能像Integer一样修改-XX:AutoBoxCacheMax=size来调整
+}
