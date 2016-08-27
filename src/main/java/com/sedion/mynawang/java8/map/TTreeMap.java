70b4d88152e49f26f8fad94140abcf79e7855838
codenewbie <18267853511@163.com>
2016-08-27 10:26:15 +0800

    TreeMap完成部分待完成红黑树后再进行学习

--- src/main/java/com/sedion/mynawang/java8/map/TTreeMap.java
+++ src/main/java/com/sedion/mynawang/java8/map/TTreeMap.java
@@ -1,9 +1,6 @@
 package com.sedion.mynawang.java8.map;
 
-import java.util.Comparator;
-import java.util.Iterator;
-import java.util.Map;
-import java.util.SortedMap;
+import java.util.*;
 
 /**
  * TreeMap源码阅读和使用（JDK1.8）
@@ -433,40 +430,56 @@ public class TTreeMap<K,V> {
      * @throws NullPointerException if the specified key is null
      *         and this map uses natural ordering, or its comparator
      *         does not permit null keys
-     *
-     *
+     * 构建排序二叉树，平衡二叉树
      *
      */
     public V put(K key, V value) {
+        // 二叉树的当前节点
         Entry<K,V> t = root;
+        // 空树，TreeMap中没有元素
         if (t == null) {
-            compare(key, key); // type (and possibly null) check
 
+            compare(key, key); // type (and possibly null) check
+            // 创建新key-value键值对赋值给root
             root = new Entry<>(key, value, null);
+            // 容器size=1表示有一个元素存在了
             size = 1;
+            // 结构修改次数+1
             modCount++;
             return null;
         }
+        // key排序的返回结果
         int cmp;
+        // 父节点
         Entry<K,V> parent;
         // split comparator and comparable paths
+        // 指定的排序算法
         Comparator<? super K> cpr = comparator;
+        // 如果cpr不为空，则用既定的排序算法进行创建TreeMap集合
         if (cpr != null) {
             do {
+                // parent指向上次循环后的t
                 parent = t;
+                // 比较新增节点的key和当前节点的key大小
                 cmp = cpr.compare(key, t.key);
+                // cmp小于0，表示新增节点的key小于当前节点的key，则以当前节点的左子节点作为新的当前节点
                 if (cmp < 0)
                     t = t.left;
+                // cmp大于0，表示新增节点的key大于当前节点的key，则以当前节点的右子节点作为新的当前节点
                 else if (cmp > 0)
                     t = t.right;
+                // cmp返回值等于0，表示两个key相等，则新值覆盖旧值，并且返回新值
                 else
                     return t.setValue(value);
             } while (t != null);
         }
+        // 如果cpr为空，则采用默认的排序算法进行创建TreeMap集合
         else {
+            // key为空报异常
             if (key == null)
                 throw new NullPointerException();
             @SuppressWarnings("unchecked")
+            // 下面算法和上面一致
             Comparable<? super K> k = (Comparable<? super K>) key;
             do {
                 parent = t;
@@ -479,16 +492,102 @@ public class TTreeMap<K,V> {
                     return t.setValue(value);
             } while (t != null);
         }
+        // 新增节点当作parent子节点
         Entry<K,V> e = new Entry<>(key, value, parent);
+        // 如果新增节点的key小于parent的key，则当做左子节点
         if (cmp < 0)
             parent.left = e;
+        // 如果新增节点的key大于parent的key，则当做右子节点
         else
             parent.right = e;
+        /*
+        *  上面已经完成了排序二叉树的的构建，将新增节点插入该树中的合适位置
+        *  下面fixAfterInsertion()方法就是对这棵树进行调整、平衡，具体过程参考上面的五种情况
+        */
         fixAfterInsertion(e);
+        // TreeMap元素数量 + 1
         size++;
+        // TreeMap容器修改次数 + 1
         modCount++;
         return null;
     }
+
+    /** From CLR */
+    /*
+     * 新增节点后的修复操作【调整二叉树的平衡】，x为新增节点
+     */
+    private void fixAfterInsertion(Entry<K,V> x) {
+        // 新增节点的颜色默认为红色
+        x.color = RED;
+        // 循环直到x不是根节点且父节点的颜色不是红色【一条路径上不能出现相邻的两个红色结点】
+        while (x != null && x != root && x.parent.color == RED) {
+            // 如果x的父节点是其父节点的父节点的左节点
+            if (parentOf(x) == leftOf(parentOf(parentOf(x)))) {
+                // 获取x的叔节点
+                Entry<K,V> y = rightOf(parentOf(parentOf(x)));
+                // 如果x的叔节点为红色
+                if (colorOf(y) == RED) {
+                    // 设置x的父节点为黑色
+                    setColor(parentOf(x), BLACK);
+                    // 设置x的叔节点为黑色
+                    setColor(y, BLACK);
+                    // 设置x的父节点的父节点为红色
+                    setColor(parentOf(parentOf(x)), RED);
+                    x = parentOf(parentOf(x));
+                }
+                // 如果x的叔节点为黑色
+                else {
+                    // 如果x是其父节点的右子树，则进行左旋转
+                    if (x == rightOf(parentOf(x))) {
+                        // 将x的父节点作为x
+                        x = parentOf(x);
+                        // 右旋转
+                        rotateLeft(x);
+                    }
+                    // 将x的父节点设为黑色
+                    setColor(parentOf(x), BLACK);
+                    // 将x的父节点的父节点设为红色
+                    setColor(parentOf(parentOf(x)), RED);
+                    // 以x的父节点的父节点作为中心进行右旋转
+                    rotateRight(parentOf(parentOf(x)));
+                }
+            }
+            // 如果X的父节点（P）是其父节点的父节点（G）的右节点
+            else {
+                // 获取X的叔节点
+                Entry<K,V> y = leftOf(parentOf(parentOf(x)));
+                // 如果X的叔节点为红色
+                if (colorOf(y) == RED) {
+                    // 设置x的父节点为黑色
+                    setColor(parentOf(x), BLACK);
+                    // 设置x的叔节点为黑色
+                    setColor(y, BLACK);
+                    // 设置x的父节点的父节点为红色
+                    setColor(parentOf(parentOf(x)), RED);
+                    x = parentOf(parentOf(x));
+                }
+                // 如果X的叔节点为黑色
+                else {
+                    // 如果X节点为其父节点的右子树，则进行左旋转
+                    if (x == leftOf(parentOf(x))) {
+                        // 将X的父节点作为X
+                        x = parentOf(x);
+                        // 右旋转
+                        rotateRight(x);
+                    }
+                    // 将X的父节点设置为黑色
+                    setColor(parentOf(x), BLACK);
+                    // 将X的父节点的父节点设置红色
+                    setColor(parentOf(parentOf(x)), RED);
+                    // 以X的父节点的父节点为中心右旋转
+                    rotateLeft(parentOf(parentOf(x)));
+                }
+            }
+        }
+        // 将根节点强制设置为黑色
+        root.color = BLACK;
+    }
+
 
     /**
      * Removes the mapping for this key from this TreeMap if present.
@@ -512,6 +611,266 @@ public class TTreeMap<K,V> {
         V oldValue = p.value;
         deleteEntry(p);
         return oldValue;
+    }
+
+    /**
+     * Returns this map's entry for the given key, or {@code null} if the map
+     * does not contain an entry for the key.
+     *
+     * @return this map's entry for the given key, or {@code null} if the map
+     *         does not contain an entry for the key
+     * @throws ClassCastException if the specified key cannot be compared
+     *         with the keys currently in the map
+     * @throws NullPointerException if the specified key is null
+     *         and this map uses natural ordering, or its comparator
+     *         does not permit null keys
+     */
+    final Entry<K,V> getEntry(Object key) {
+        // Offload comparator-based version for sake of performance
+        if (comparator != null)
+            return getEntryUsingComparator(key);
+        if (key == null)
+            throw new NullPointerException();
+        @SuppressWarnings("unchecked")
+        Comparable<? super K> k = (Comparable<? super K>) key;
+        Entry<K,V> p = root;
+        while (p != null) {
+            int cmp = k.compareTo(p.key);
+            if (cmp < 0)
+                p = p.left;
+            else if (cmp > 0)
+                p = p.right;
+            else
+                return p;
+        }
+        return null;
+    }
+
+    /**
+     * Version of getEntry using comparator. Split off from getEntry
+     * for performance. (This is not worth doing for most methods,
+     * that are less dependent on comparator performance, but is
+     * worthwhile here.)
+     */
+    final Entry<K,V> getEntryUsingComparator(Object key) {
+        @SuppressWarnings("unchecked")
+        K k = (K) key;
+        Comparator<? super K> cpr = comparator;
+        if (cpr != null) {
+            Entry<K,V> p = root;
+            while (p != null) {
+                int cmp = cpr.compare(k, p.key);
+                if (cmp < 0)
+                    p = p.left;
+                else if (cmp > 0)
+                    p = p.right;
+                else
+                    return p;
+            }
+        }
+        return null;
+    }
+
+    /**
+     * Delete node p, and then rebalance the tree.
+     */
+    private void deleteEntry(Entry<K,V> p) {
+        modCount++;
+        size--;
+
+        // If strictly internal, copy successor's element to p and then make p
+        // point to successor.
+        if (p.left != null && p.right != null) {
+            Entry<K,V> s = successor(p);
+            p.key = s.key;
+            p.value = s.value;
+            p = s;
+        } // p has 2 children
+
+        // Start fixup at replacement node, if it exists.
+        Entry<K,V> replacement = (p.left != null ? p.left : p.right);
+
+        if (replacement != null) {
+            // Link replacement to parent
+            replacement.parent = p.parent;
+            if (p.parent == null)
+                root = replacement;
+            else if (p == p.parent.left)
+                p.parent.left  = replacement;
+            else
+                p.parent.right = replacement;
+
+            // Null out links so they are OK to use by fixAfterDeletion.
+            p.left = p.right = p.parent = null;
+
+            // Fix replacement
+            if (p.color == BLACK)
+                fixAfterDeletion(replacement);
+        } else if (p.parent == null) { // return if we are the only node.
+            root = null;
+        } else { //  No children. Use self as phantom replacement and unlink.
+            if (p.color == BLACK)
+                fixAfterDeletion(p);
+
+            if (p.parent != null) {
+                if (p == p.parent.left)
+                    p.parent.left = null;
+                else if (p == p.parent.right)
+                    p.parent.right = null;
+                p.parent = null;
+            }
+        }
+    }
+
+    /**
+     * Returns the successor of the specified Entry, or null if no such.
+     */
+    static <K,V> TTreeMap.Entry<K,V> successor(Entry<K,V> t) {
+        if (t == null)
+            return null;
+        else if (t.right != null) {
+            Entry<K,V> p = t.right;
+            while (p.left != null)
+                p = p.left;
+            return p;
+        } else {
+            Entry<K,V> p = t.parent;
+            Entry<K,V> ch = t;
+            while (p != null && ch == p.right) {
+                ch = p;
+                p = p.parent;
+            }
+            return p;
+        }
+    }
+
+    /** From CLR */
+    private void fixAfterDeletion(Entry<K,V> x) {
+        while (x != root && colorOf(x) == BLACK) {
+            if (x == leftOf(parentOf(x))) {
+                Entry<K,V> sib = rightOf(parentOf(x));
+
+                if (colorOf(sib) == RED) {
+                    setColor(sib, BLACK);
+                    setColor(parentOf(x), RED);
+                    rotateLeft(parentOf(x));
+                    sib = rightOf(parentOf(x));
+                }
+
+                if (colorOf(leftOf(sib))  == BLACK &&
+                        colorOf(rightOf(sib)) == BLACK) {
+                    setColor(sib, RED);
+                    x = parentOf(x);
+                } else {
+                    if (colorOf(rightOf(sib)) == BLACK) {
+                        setColor(leftOf(sib), BLACK);
+                        setColor(sib, RED);
+                        rotateRight(sib);
+                        sib = rightOf(parentOf(x));
+                    }
+                    setColor(sib, colorOf(parentOf(x)));
+                    setColor(parentOf(x), BLACK);
+                    setColor(rightOf(sib), BLACK);
+                    rotateLeft(parentOf(x));
+                    x = root;
+                }
+            } else { // symmetric
+                Entry<K,V> sib = leftOf(parentOf(x));
+
+                if (colorOf(sib) == RED) {
+                    setColor(sib, BLACK);
+                    setColor(parentOf(x), RED);
+                    rotateRight(parentOf(x));
+                    sib = leftOf(parentOf(x));
+                }
+
+                if (colorOf(rightOf(sib)) == BLACK &&
+                        colorOf(leftOf(sib)) == BLACK) {
+                    setColor(sib, RED);
+                    x = parentOf(x);
+                } else {
+                    if (colorOf(leftOf(sib)) == BLACK) {
+                        setColor(rightOf(sib), BLACK);
+                        setColor(sib, RED);
+                        rotateLeft(sib);
+                        sib = leftOf(parentOf(x));
+                    }
+                    setColor(sib, colorOf(parentOf(x)));
+                    setColor(parentOf(x), BLACK);
+                    setColor(leftOf(sib), BLACK);
+                    rotateRight(parentOf(x));
+                    x = root;
+                }
+            }
+        }
+
+        setColor(x, BLACK);
+    }
+
+
+
+
+    final int compare(Object k1, Object k2) {
+        return comparator==null ? ((Comparable<? super K>)k1).compareTo((K)k2)
+                : comparator.compare((K)k1, (K)k2);
+    }
+
+
+    private static <K,V> boolean colorOf(Entry<K,V> p) {
+        return (p == null ? BLACK : p.color);
+    }
+
+    private static <K,V> Entry<K,V> parentOf(Entry<K,V> p) {
+        return (p == null ? null: p.parent);
+    }
+
+    private static <K,V> void setColor(Entry<K,V> p, boolean c) {
+        if (p != null)
+            p.color = c;
+    }
+
+    private static <K,V> Entry<K,V> leftOf(Entry<K,V> p) {
+        return (p == null) ? null: p.left;
+    }
+
+    private static <K,V> Entry<K,V> rightOf(Entry<K,V> p) {
+        return (p == null) ? null: p.right;
+    }
+
+    /** From CLR */
+    private void rotateLeft(Entry<K,V> p) {
+        if (p != null) {
+            Entry<K,V> r = p.right;
+            p.right = r.left;
+            if (r.left != null)
+                r.left.parent = p;
+            r.parent = p.parent;
+            if (p.parent == null)
+                root = r;
+            else if (p.parent.left == p)
+                p.parent.left = r;
+            else
+                p.parent.right = r;
+            r.left = p;
+            p.parent = r;
+        }
+    }
+
+    /** From CLR */
+    private void rotateRight(Entry<K,V> p) {
+        if (p != null) {
+            Entry<K,V> l = p.left;
+            p.left = l.right;
+            if (l.right != null) l.right.parent = p;
+            l.parent = p.parent;
+            if (p.parent == null)
+                root = l;
+            else if (p.parent.right == p)
+                p.parent.right = l;
+            else p.parent.left = l;
+            l.right = p;
+            p.parent = l;
+        }
     }
 
 
